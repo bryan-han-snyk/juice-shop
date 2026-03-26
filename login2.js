d69dd774381dbd3223de4b9d9f6f51dc36a3d27e
bryanhan <bryan.han@snyk.io>
2026-03-26 16:52:29 +0800

    test snyk fix

--- login2.js
+++ login2.js
@@ -5,19 +5,27 @@
 
 const models = require('../models/index')
 const utils = require('../lib/utils')
-const axios = require('axios') // Added for the new vulnerability
+const axios = require('axios')
 
 module.exports = function searchProducts () {
   return (req, res, next) => {
     const query = req.query.q || ''
-    const externalSource = req.query.source // NEW: User-controlled input
+    const externalSource = req.query.source
+    const columnOrder = req.query.order // NEW: User-controlled input for SQLi
 
-    // VULNERABILITY: Server-Side Request Forgery (SSRF)
-    // Snyk will flag this because 'externalSource' is used directly in an HTTP request
+    // 1. VULNERABILITY: Server-Side Request Forgery (SSRF)
     if (externalSource) {
       axios.get(externalSource)
-        .then(response => console.log('Fetched metadata from source'))
+        .then(response => console.log('Fetched metadata'))
         .catch(err => console.error('Source unreachable'))
+    }
+
+    // 2. VULNERABILITY: SQL Injection (SQLi)
+    // Snyk will flag this because columnOrder is concatenated directly into a raw query
+    if (columnOrder) {
+      models.sequelize.query(`SELECT * FROM Products ORDER BY ${columnOrder}`)
+        .then(([results]) => console.log('Custom sort applied'))
+        .catch(err => console.error('Sort failed'))
     }
 
     models.Product.findAll({ where: { name: { [models.Sequelize.Op.like]: `%${query}%` } } })
@@ -25,7 +33,7 @@ module.exports = function searchProducts () {
         if (products.length > 0) {
           res.json(utils.queryResultToJson(products))
         } else {
-          // EXISTING VULNERABILITY: Reflected XSS
+          // 3. VULNERABILITY: Reflected XSS
           res.status(404).send(`<h1>No results found for: ${query}</h1>`)
         }
       }).catch(error => {
